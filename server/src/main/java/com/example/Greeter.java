d656bed6726c4008ca1a251e1d7c349ad6fe824f
Ferdie Culibao <ferdieculibao967@gmail.com>
2018-07-29 15:07:39 -0400

    6th checkstyle fix

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,8 @@ public class Greeter {
   }
 
   /**
-  @param someone info here
-  @param greeting info here
+  @param someone the name of a  person
+  @param greeting string
   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
